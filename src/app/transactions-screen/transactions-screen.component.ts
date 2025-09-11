16984351bb61b7c122364f8908fd856402c8c7c2
Adi Saric <adi.saric02gmail.com>
2025-09-11 12:28:27 +0200

    navigating to screen

---
+++ src/app/transactions-screen/transactions-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions-screen',
+  imports: [],
+  templateUrl: './transactions-screen.component.html',
+  styleUrl: './transactions-screen.component.scss'
+})
+export class TransactionsScreenComponent {
+
+}
