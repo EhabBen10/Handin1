a52f43903696f1d1229f5a6b2568ff63cd92afb7
Adi Saric <adi.saric02gmail.com>
2025-09-15 11:21:48 +0200

    bop

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -1,18 +1,22 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Transaction } from './creditcards.service';
 import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
   private httpClient = inject(HttpClient);
+  private authService = inject(AuthService);
   private apiUrl = `${environment.apiBase}/api/Transaction`;
 
-  public getTransactions(token?: string): Observable<Transaction[]> {
-    const headers = token ? new HttpHeaders({ Authorization: `Bearer ${token}` }) : undefined;
-    return this.httpClient.get<Transaction[]>(this.apiUrl, { headers });
+  public getTransactions(): Observable<Transaction[]> {
+    const token = this.authService.getAccessToken();
+    return this.httpClient.get<Transaction[]>(this.apiUrl, {
+      headers: token ? { Authorization: `Bearer ${token}` } : {}
+    });
   }
 }
--- src/app/transactions-screen/transactions-screen.component.ts
+++ src/app/transactions-screen/transactions-screen.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { TransactionsListComponent } from './transactions-list/transactions-list.component';
 import { Transaction } from '../services/creditcards.service';
 import { TransactionsService } from '../services/transactions.service';
-import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-transactions-screen',
@@ -12,24 +11,13 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './transactions-screen.component.html',
   styleUrls: ['./transactions-screen.component.scss']
 })
-
 export class TransactionsScreenComponent implements OnInit {
   private transactionsService = inject(TransactionsService);
-  private authService = inject(AuthService);
-
   transactions = signal<Transaction[]>([]);
 
-  ngOnInit(): void {
-    const token = this.authService.getAccessToken(); 
-
-    if (!token) {
-      console.error('No access token available');
-      return;
-    }
-
-    this.transactionsService.getTransactions(token).subscribe({
-      next: (data) => this.transactions.set(data),
-      error: (err) => console.error('Failed to load transactions', err)
+  ngOnInit() {
+    this.transactionsService.getTransactions().subscribe(transactions => {
+      this.transactions.set(transactions);
     });
   }
 }
