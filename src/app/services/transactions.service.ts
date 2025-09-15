71d31100683b64ed6d0ba05dd04caab9b26cc097
Adi Saric <adi.saric02gmail.com>
2025-09-15 11:25:09 +0200

    cleaned service

--- src/app/services/transactions.service.ts
+++ src/app/services/transactions.service.ts
@@ -1,22 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Transaction } from './creditcards.service';
 import { environment } from '../../environments/environment';
-import { AuthService } from './auth.service';
+import { Transaction } from './creditcards.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionsService {
   private httpClient = inject(HttpClient);
-  private authService = inject(AuthService);
-  private apiUrl = `${environment.apiBase}/api/Transaction`;
 
   public getTransactions(): Observable<Transaction[]> {
-    const token = this.authService.getAccessToken();
-    return this.httpClient.get<Transaction[]>(this.apiUrl, {
-      headers: token ? { Authorization: `Bearer ${token}` } : {}
-    });
+    return this.httpClient.get<Transaction[]>(`${environment.apiBase}/api/Transaction`);
   }
 }
